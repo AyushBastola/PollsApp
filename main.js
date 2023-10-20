295cd55632c49816989a17c6afba23ce20b077c1
AyushBastola <144130544+AyushBastola@users.noreply.github.com>
2023-10-20 12:39:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const poll = document.querySelector('.poll')
+const buttons = poll.querySelectorAll('button')
+
+buttons[0].addEventListener('click', Vote);
+buttons[1].addEventListener('click', Vote);
+buttons[2].addEventListener('click', Vote);
+buttons[3].addEventListener('click', Vote);
+
+
+function Vote(e) {
+    console.log(e.target.innerHTML);
+}
+
